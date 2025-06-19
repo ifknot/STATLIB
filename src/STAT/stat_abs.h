1b12869f4c2afbc6678aed6ff0f9f483142d6b7d
ifknot <ifknot@hotmail.com>
2025-06-19 15:15:31 +0100

    Update stat_abs.h

--- src/STAT/stat_abs.h
+++ src/STAT/stat_abs.h
@@ -4,14 +4,68 @@
 #include "stat_types.h"
 
 
-// Floating-point absolute value
-stat_float_t stat_abs_f(stat_float_t value);
+/**
+ * @brief Compute absolute values of float array (in-place)
+ * @param values Array of values (can be length 1)
+ * @param count Number of elements
+ */
+void stat_abs_f(stat_float_t* values, stat_size_t count);
 
-// Signed 32-bit absolute value
-int32_t stat_abs_i32(int32_t value);
-bool stat_safe_abs_i32(int32_t value, int32_t* result);
+/**
+ * @brief Compute absolute values of int32 array (in-place)
+ * @param values Array to modify (can be length 1)
+ * @param count Number of elements
+ * @return Number of successful conversions (count-INT32_MIN occurrences)
+ */
+stat_size_t stat_abs_i32(stat_int_t* values, stat_size_t count)
 
-// Unsigned 32-bit absolute value (identity function)
-uint32_t stat_abs_u32(uint32_t value);
+/**
+ * @brief Safe absolute values (out-of-place)
+ * @param input Source array
+ * @param output Destination array (pre-allocated)
+ * @param count Number of elements
+ * @return Number of successful conversions
+ */
+stat_size_t stat_safe_abs_i32(
+    const stat_int_t* input, 
+    stat_int_t* output, 
+    stat_size_t count
+)
+
+/**
+ * @brief Scalar wrapper for stat_abs_f (float)
+ * @param x Single value to process
+ * @return Absolute value
+ */
+static inline stat_float_t stat_abs_scalar_f(stat_float_t x) {
+    stat_abs_f(&x, 1);
+    return x;
+}
+
+/**
+ * @brief Scalar wrapper for stat_abs_i32 (int32)
+ * @param x Single value to process
+ * @return Absolute value (undefined if x=INT32_MIN)
+ */
+static inline stat_int_t stat_abs_scalar_i32(stat_int_t x) {
+    stat_abs_i32(&x, 1);
+    return x;
+}
+
+/**
+ * @brief Safe scalar wrapper for stat_safe_abs_i32
+ * @param x Single value to process
+ * @param result Output pointer (unchanged if x=INT32_MIN)
+ * @return True if successful, False if x=INT32_MIN
+ */
+static inline bool stat_safe_abs_scalar_i32(stat_int_t x, stat_int_t* result) {
+    stat_int_t tmp = x;
+    if (stat_abs_i32(&tmp, 1) == 0) {
+        return false;
+    }
+    *result = tmp;
+    return true;
+}
+
 
 #endif // STAT_ABS_H
