6d1b5898d18ef84d2a55bf4d397b66c6fd7b71bd
ifknot <ifknot@hotmail.com>
2025-06-20 15:31:17 +0100

    Update stat_outliers.h

--- src/STAT/stat_outliers.h
+++ src/STAT/stat_outliers.h
@@ -23,26 +23,26 @@ bool stat_is_outlier(stat_float_t value, const stat_five_num_summary_t* summary)
 stat_size_t stat_count_outliers(const stat_float_t* data, stat_size_t size, const stat_five_num_summary_t* summary);
 
 /**
- * @brief Determines which values in an array are outliers (array version)
+ * @brief Flags outliers in an array by setting boolean indicators
  * 
  * @param[in] values Input array of values
  * @param[in] count Number of values in input array
  * @param[in] summary Five-number summary reference
- * @param[out] results Output array for outlier flags (must be pre-allocated)
- * @return bool* Pointer to results array for chaining
+ * @param[out] flags Output array for outlier flags (must be pre-allocated)
+ * @return stat_size_t Number of outliers found
  */
-stat_size_t stat_outliers_as_array(const stat_float_t* values, stat_size_t count, const stat_five_num_summary_t* summary, bool* outliers);
+stat_size_t stat_flag_outliers(const stat_float_t* values, stat_size_t count, const stat_five_num_summary_t* summary, bool* flags);
 
 /**
- * @brief Enumerates outliers by storing pointers to them in a destination array
+ * @brief Collects outlier values into a destination array
  * 
- * @param[in] values Input array of values
- * @param[in] count Number of values in input array
+ * @param[in] src Input array of values
+ * @param[in] src_count Number of values in input array
  * @param[in] summary Five-number summary reference
- * @param[out] dest Destination array for outlier pointers (must be pre-allocated)
- * @return stat_size_t Number of outliers found (may be less than array capacity)
+ * @param[out] dst Destination array for outlier values (must be pre-allocated)
+ * @return stat_size_t Number of outliers collected
  */
-stat_size_t stat_enumerate_outliers(const stat_float_t* src, stat_size_t src_count, const stat_five_num_summary_t* summary, stat_float_t* dst);
+stat_size_t stat_collect_outliers(const stat_float_t* values, stat_size_t src_count, const stat_five_num_summary_t* summary, stat_float_t* outliers);
 
 
 #endif 
