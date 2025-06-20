3ebd638b8484b35575d13a4109377d2037e997ac
ifknot <ifknot@hotmail.com>
2025-06-20 14:55:34 +0100

    Update stat_outliers.h

--- src/STAT/stat_outliers.h
+++ src/STAT/stat_outliers.h
@@ -20,17 +20,7 @@ bool stat_is_outlier(stat_float_t value, const stat_five_num_summary_t* summary)
  * @param summary Pointer to the five-number summary
  * @return stat_size_t Number of outliers found
  */
-stat_size_t stat_count_outliers(const stat_float_t* data, stat_size_t size, 
-                               const stat_five_num_summary_t* summary);
-
-/**
- * @brief Determines if a value is an outlier based on a five-number summary
- * 
- * @param value The value to check
- * @param summary Pointer to the five-number summary
- * @return true if the value is an outlier, false otherwise
- */
-bool stat_is_outlier(stat_float_t value, const stat_five_num_summary_t* summary);
+stat_size_t stat_count_outliers(const stat_float_t* data, stat_size_t size, const stat_five_num_summary_t* summary);
 
 /**
  * @brief Determines which values in an array are outliers (array version)
@@ -42,16 +32,6 @@ bool stat_is_outlier(stat_float_t value, const stat_five_num_summary_t* summary)
  * @return bool* Pointer to results array for chaining
  */
 bool* stat_is_outlier_array(const stat_float_t* values, stat_size_t count, const stat_five_num_summary_t* summary, bool* results);
-
-/**
- * @brief Counts outliers in an array (array version)
- * 
- * @param[in] values Input array of values
- * @param[in] count Number of values in input array
- * @param[in] summary Five-number summary reference
- * @return stat_size_t Count of outliers found
- */
-stat_size_t stat_count_outliers_array(const stat_float_t* values, stat_size_t count, const stat_five_num_summary_t* summary);
 
 /**
  * @brief Enumerates outliers by storing pointers to them in a destination array
