3c916e49590a3f180fba014471c2c3b46d20573d
ifknot <ifknot@hotmail.com>
2025-06-27 14:13:52 +0100

    Update stat_basic.c

--- src/STAT/stat_basic.c
+++ src/STAT/stat_basic.c
@@ -1,87 +1,103 @@
 #include "stat_basic.h"
-
 #include <assert.h>
 #include <errno.h>
 #include <math.h>
-#include <stddef.h>
-#include <stdint.h>
+#include <limits.h>
 
-stat_float_t stat_min_f(const stat_float_t* data, stat_size_t count) {
-    assert(data != NULL && "Input array cannot be NULL");
+stat_float_t stat_min_float_array(const stat_float_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
         return NAN;
     }
 
-    stat_float_t min = data[0];
+    stat_float_t min = source[0];
     for (stat_size_t i = 1; i < count; i++) {
-        if (data[i] < min) min = data[i];
+        if (source[i] < min) min = source[i];
     }
     return min;
 }
 
-stat_float_t stat_max_f(const stat_float_t* data, stat_size_t count) {
-    assert(data != NULL && "Input array cannot be NULL");
+stat_float_t stat_max_float_array(const stat_float_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
         return NAN;
     }
 
-    stat_float_t max = data[0];
+    stat_float_t max = source[0];
     for (stat_size_t i = 1; i < count; i++) {
-        if (data[i] > max) max = data[i];
+        if (source[i] > max) max = source[i];
     }
     return max;
 }
 
-stat_float_t stat_range_f(const stat_float_t* data, stat_size_t count) {
-    assert(data != NULL && "Input array cannot be NULL");
+stat_float_t stat_range_float_array(const stat_float_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
         return NAN;
     }
-    return stat_max_f(data, count) - stat_min_f(data, count);
+    return stat_max_float_array(source, count) - stat_min_float_array(source, count);
 }
 
-stat_int_t stat_max_i(const stat_int_t* values, stat_size_t count) {
-    assert(values != NULL && "Input array cannot be NULL");
+stat_int_t stat_max_int_array(const stat_int_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
         return INT32_MIN;
     }
 
-    stat_int_t max = values[0];
+    stat_int_t max = source[0];
     for (stat_size_t i = 1; i < count; i++) {
-        if (values[i] > max) max = values[i];
+        if (source[i] > max) max = source[i];
     }
     return max;
 }
 
-stat_int_t stat_min_i(const stat_int_t* values, stat_size_t count) {
-    assert(values != NULL && "Input array cannot be NULL");
+stat_int_t stat_min_int_array(const stat_int_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
-        return INT32_MIN;
+        return INT32_MAX;
     }
 
-    stat_int_t min = values[0];
+    stat_int_t min = source[0];
     for (stat_size_t i = 1; i < count; i++) {
-        if (values[i] < min) min = values[i];
+        if (source[i] < min) min = source[i];
     }
     return min;
 }
 
-stat_int_t stat_range_i(const stat_int_t *data, stat_size_t count) {
-    assert(data != NULL && "Input array cannot be NULL");
+stat_int_t stat_range_int_array(const stat_int_t* source, stat_size_t count) {
+    assert(source != NULL && "Input array cannot be NULL");
 
     if (count == 0) {
         errno = EINVAL;
         return INT32_MIN;
     }
-    return stat_max_i(data, count) - stat_min_i(data, count);
+    return stat_max_int_array(source, count) - stat_min_int_array(source, count);
+}
+
+stat_int_t* stat_cast_float_to_int_array(stat_int_t* destination, const stat_float_t* source, stat_size_t count) {
+    assert(destination != NULL && source != NULL && "Arrays cannot be NULL");
+    
+    for (stat_size_t i = 0; i < count; i++) {
+        destination[i] = (stat_int_t)source[i];
+    }
+    return destination;
+}
+
+stat_float_t* stat_cast_int_to_float_array(stat_float_t* destination, const stat_int_t* source, stat_size_t count) {
+    assert(destination != NULL && source != NULL && "Arrays cannot be NULL");
+    
+    for (stat_size_t i = 0; i < count; i++) {
+        destination[i] = (stat_float_t)source[i];
+    }
+    return destination;
 }
